3b19f86d67864b41295693012afb16a09555c296
SomeKittens <rkoutnik@gmail.com>
2013-01-07 12:44:42 -0500

    Refactored for the usual reasons

--- mongoHelper.js
+++ mongoHelper.js
@@ -11,70 +11,45 @@ var mongo = require('mongodb')
  * completed.  The results of the find query (if applicable) will be passed to it
  * AFTER the database is closed, so you can execute another db call.
  */
- 
-function getCollection(coll, callback) {
+
+//Takes care of rote work that every operation needs
+//coll is the name of the collection we're operating on
+//next is an optional callback.  If the operation returns results, they'll be 
+//    passed to next.
+//operation contains the db operation that we want to execute
+function getCollection(coll, next, operation) {
   mdb.open(function(err, db) {
-    db.collection(coll, callback);
+    db.collection(coll, function(err, collection) {
+      operation(err, collection, function(err, results) {
+        if(err) { console.error(err); }
+        db.close();
+        if(typeof next === 'function') {
+          next(results);
+        }
+      });
+    });
   });
-}
-
-function cleanup(next, db) {
-  return function() {
-    db.close();
-    if(typeof next === 'function') {
-      next();
-    }
-  };
-}
-
-function error(err, db) {
-  if(err) {
-    console.error(err);
-    db.close();
-  }
 }
 
 module.exports = {
   insert: function(coll, query, next) {
-    mdb.open(function(err, db) {
-      db.collection(coll, function(err, collection) {
-        collection.insert(query, function() {
-          db.close();
-          if(typeof next === 'function') { next(); }
-        });
-      });
+    getCollection(coll, next, function(err, collection, after) {
+      collection.insert(query, after);
     });
   },
   find: function(coll, query, next) {
-    mdb.open(function(err, db) {
-      db.collection(coll, function(err, collection) {
-        collection.find(query).toArray(function(err, results) {
-          if(err) { console.error(err);db.close();return; }
-          db.close();
-          if(typeof next === 'function') { next(results); }
-        });
-      });
+    getCollection(coll, next, function(err, collection, after) {
+      collection.find(query).toArray(after);
     });
   },
   update: function(coll, criteria, update, params, next) {
-    mdb.open(function(err, db) {
-      db.collection(coll, function(err, collection) {
-        collection.update(criteria, update, params, function(err, updated) {
-          if(err) { console.error(err);db.close();return; }
-          db.close();
-          if(typeof next === 'function') { next(updated); }
-        });
-      });
+    getCollection(coll, next, function(err, collection, after) {
+      collection.update(criteria, update, params, after);
     });
   },
   remove: function(coll, query, next) {
-    mdb.open(function(err, db) {
-      db.collection(coll, function(err, collection) {
-        collection.remove(query, function(err, removed) {
-          db.close();
-          if(typeof next === 'function') { next(removed); }
-        });
-      });
+    getCollection(coll, next, function(err, collection, after) {
+      collection.remove(query, after);
     });
   }
 };
